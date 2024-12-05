a237c28f554a20f0194363093844174c43008506
sudhanshu <sudhanshu1391.be21@chitkara.edu.in>
2024-12-05 13:29:26 +0530

    [sudhanshu] ADD: calculate daily wage

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 #Employee_Wage
 
 UC_1 - Check Attendance system via Math.random()
+UC_2 - Ability to Calculate Daily
+Employee Wage based on
+part time or full time work
--- employee_wage.js
+++ employee_wage.js
@@ -1,20 +1,48 @@
-//Ability to Check Employee is present or Absent
 
 
 let a=Math.floor(Math.random()*2);
-
 if(a===1){
-    console.log("employee is present");
+    present();
 }
-else if(a===0){
-    console.log("employee is absent");
+if(a===0){
+    console.log("absent");
 }
 
+function present(){
+
+let part_hours=4;
+let full_hours=8;
+let hour_wage=20;
 
 
+let workType = Math.floor(Math.random() * 3);
+
+let dailyWage=0;
+
+switch (workType){
+
+    case 0:
+        console.log("notime worked hours 0");
+        dailyWage=0;
+
+        case 1:
+            console.log("partime worked hours is 4");
+            dailyWage=part_hours*hour_wage;
+            break;
+
+            case 2:
+                console.log("fulltime worked hours is 8");
+                dailyWage=full_hours*hour_wage;
+                break;
+
+                default:
+                    console.log("invalid hours");
+                    dailyWage=0;
+                    break;
 
 
-
-
-
+        
+}
+console.log(dailyWage);
+}
 
