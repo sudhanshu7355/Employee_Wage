40fa0608a587d01cab9f2a7b22b7c39af7a8c1a0
sudhanshu <sudhanshu1391.be21@chitkara.edu.in>
2024-12-05 13:45:40 +0530

    [sudhanshu] ADD: Refactor the Code to write a function to get work hours

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 #Employee_Wage
 
 UC_1 - Check Attendance system via Math.random()
+
 UC_2 - Ability to Calculate Daily
 Employee Wage based on
 part time or full time work
+
+UC_3 - Refactor the Code to write a function to get work hours
--- employee_wage.js
+++ employee_wage.js
@@ -8,11 +8,13 @@ if(a===0){
     console.log("absent");
 }
 
+
 function present(){
 
 let part_hours=4;
 let full_hours=8;
 let hour_wage=20;
+total_hours=0;
 
 
 let workType = Math.floor(Math.random() * 3);
@@ -23,15 +25,18 @@ switch (workType){
 
     case 0:
         console.log("notime worked hours 0");
+        total_hours=0;
         dailyWage=0;
 
         case 1:
             console.log("partime worked hours is 4");
+            total_hours=total_hours+part_hours;
             dailyWage=part_hours*hour_wage;
             break;
 
             case 2:
                 console.log("fulltime worked hours is 8");
+                total_hours=total_hours+full_hours;
                 dailyWage=full_hours*hour_wage;
                 break;
 
